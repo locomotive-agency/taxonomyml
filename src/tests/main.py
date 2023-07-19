502a1483f69e33ad1c3338dcdac3025a0beee3ab
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2023-07-19 17:46:58 +0100

    Re-add tests

---
+++ src/tests/main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import unittest
+from main import get_data
+
+
+class TestData(unittest.TestCase):
+    def test_get_data(self):
+        # Test with a pandas dataframe
+        data = pd.DataFrame({"text": ["hello", "world"], "search_volume": [100, 200]})
+        result = get_data(
+            data=data, text_column="text", search_volume_column="search_volume"
+        )
+        self.assertEqual(result.shape, (2, 2))
+
+        # Test with Google Search Console data
+        data = "sc-domain:example.com"
+        result = get_data(data=data, days=7, brand_terms=["example"], limit_queries=10)
+        self.assertGreater(result.shape[0], 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
